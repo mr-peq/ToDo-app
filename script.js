23607262141c390a58582cc90883fab25acd129f
mr-peq <theodore1712@yahoo.fr>
2023-08-08 14:37:51 +0200

    Add scheduled items and styled items on hover (CSS)

--- script.js
+++ script.js
@@ -4,69 +4,117 @@ document.addEventListener('click', helper);     // => Event listener sur tout le
 document.addEventListener('click', deleteItem); // => En revanche, celui du "delete-button" DOIT être sur le document car il n'existe pas encore tant que
                                                 //.. l'utilisateur n'a pas créé un nouvel "item"
 
-let currentTarget;                              
-let currentHelper;
+let ulTarget;                  // => Le <ul> le plus proche du "add-button" cliqué (là où on va placer l'item)                            
+let currentHelper;                  // => Pour sauvegarder le fait qu'un helper est présent (bloque les autres 'events')
+let liScheduled = [];               // => La liste des items "scheduled"
+let currentCategory;                // => 'section' dans laquelle on veut ajouter un item (important, normal, scheduled)
 
 
-
+/* ================================== HELPER ================================== */
 
 function helper(event){
     if(currentHelper != undefined) return;
 
     if(event.target.classList.contains('add-button')){
-        currentTarget = event.target.parentElement.nextElementSibling;     // => On récupère le <ul> le + proche (voir HTML)
+        ulTarget = event.target.parentElement.nextElementSibling;     // => On récupère le <ul> le + proche (voir HTML)
+        currentCategory = event.target.closest('section');                    /* ++++ */ // => On récupère la section dans laquelle est situé le 'add-button' 
 
+        let helper = buildHelper();
 
-        let helper = document.createElement('div');                 // => On crée le "helper" (= fenêtre de création du nouvel item)
-        helper.classList.add('helper');
+        if(currentCategory.id == "scheduled"){
+            let date = document.createElement('input');                 /* ++++ */
+            date.type = "datetime-local";
+            date.classList.add('helper-date');
 
-        let input = document.createElement('textarea');                  // => On crée l'espace pour entrer le texte ("input"), le bouton OK ("confirm") et celui
-        input.classList.add('helper-input');                        //.. d'annulation("cancel")
-        input.placeholder = "Enter text here...";
-        
-        let confirm = document.createElement('div');
-        confirm.classList.add('helper-confirm');
-        confirm.innerHTML = "OK";
+            helper.insertAdjacentElement('afterbegin', date);
+        }
 
-        let cancel = document.createElement('div');
-        cancel.classList.add('helper-cancel');
-        cancel.innerHTML = "CANCEL";
-
-
-        helper.insertAdjacentElement('afterbegin', input);          // => On ajoute "input", "confirm" et "cancel" au "Helper"
-        helper.insertAdjacentElement('beforeend', confirm);
-        helper.insertAdjacentElement('beforeend', cancel);
-
-        document.body.append(helper);
+        currentCategory.append(helper);                                    /* ++++ */
         helper.focus();
         currentHelper = helper;
+
+        let confirm = document.querySelector('.helper-confirm');
+        let cancel = document.querySelector('.helper-cancel');
 
         confirm.addEventListener('click', createItem);
         cancel.addEventListener('click', cancelCreation); 
     }
+
     else return;   
     
 }
 
+function buildHelper(){
+
+    let helper = document.createElement('div');                 // => On crée le "helper" (= fenêtre de création du nouvel item)
+    helper.classList.add('helper');
+
+    let input = document.createElement('textarea');                  // => On crée l'espace pour entrer le texte ("input"), le bouton OK ("confirm") et celui
+    input.classList.add('helper-input');                        //.. d'annulation("cancel")
+    input.placeholder = "Enter text here...";
+        
+    let confirm = document.createElement('div');
+    confirm.classList.add('helper-confirm');
+    confirm.innerHTML = "OK";
+
+    let cancel = document.createElement('div');
+    cancel.classList.add('helper-cancel');
+    cancel.innerHTML = "CANCEL";
+
+
+    helper.insertAdjacentElement('afterbegin', input);          // => On ajoute "input", "confirm" et "cancel" au "Helper"
+    helper.insertAdjacentElement('beforeend', confirm);
+    helper.insertAdjacentElement('beforeend', cancel);
+
+    return helper;
+}
+
+
+/* ================================== ITEMS ================================== */
 
 function createItem(){
-    let input = document.querySelector('.helper-input');
 
-    let item = document.createElement('div');
+    let input = document.querySelector('.helper-input');        // => Le 'textarea'
+
+    let item = document.createElement('div');                   // => On crée le "item" (contenant le 'li'( = texte) et le "delete-button")
     item.classList.add('item');
 
+
     let li = document.createElement('li');
-    li.innerHTML = input.value;
+    li.textContent = input.value;
+
     let deleteButton = document.createElement('div');
     deleteButton.classList.add('delete-button');
     deleteButton.innerHTML = "X";
 
+    
 
     item.insertAdjacentElement('afterbegin', li);
     item.insertAdjacentElement('beforeend', deleteButton);
     
-    currentTarget.append(item);
-    
+    ulTarget.append(item);
+
+    if(currentCategory.id == 'scheduled'){
+        let getdate = document.querySelector('.helper-date');       /* ++++ */
+        const chars = {
+            '-': '/',
+            'T': '  '
+        }
+
+        let date = document.createElement('b');
+        date.textContent = getdate.value.replace(/[-T]/g, letter => chars[letter]);
+
+        
+        li.textContent = ' => ' + input.value;
+        li.insertAdjacentElement('afterbegin', date);
+
+        liScheduled.push(item);                                         /* ++++ */
+        liScheduled.sort((a, b) => a.textContent > b.textContent);
+        for(let elem of liScheduled){
+            ulTarget.append(elem);
+        }
+    }
+        
     currentHelper.remove();
     currentHelper = undefined;
 }
